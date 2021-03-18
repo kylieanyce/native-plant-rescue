6b9afd7c88fa69a4bd723686fc498e16cfb068d2
kylieanyce <kylieanyce@gmail.com>
2021-03-18 14:41:27 -0500

    switched title of plant cards

--- src/components/plants/SelectPlant.js
+++ src/components/plants/SelectPlant.js
@@ -7,8 +7,8 @@ export const SelectPlantCard = ({ plant }) => {
 
     return (
         <div value={plant.id}>
-            <h4>Common Name: {plant.plant_details.common_names}</h4>
-            <p>Scientific Name: {plant.plant_details.scientific_name}</p>
+            <h4>Scientific Name: {plant.plant_details.scientific_name}</h4>
+            <p>Common Name: {plant.plant_details.common_names}</p>
             <p>{plant.plant_details.wiki_description.value}</p>
             <img src={plant.similar_images[0].url}></img>
             <button onClick={() => history.push("/create")}>This is my plant!</button>
