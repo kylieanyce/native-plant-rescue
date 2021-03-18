2526a80878ae1f7ac13431d1b210d51e73192f0b
kylieanyce <kylieanyce@gmail.com>
2021-03-18 14:12:10 -0500

    plant photos render to DOM

--- src/components/plants/SelectPlant.js
+++ src/components/plants/SelectPlant.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
+
 
 export const SelectPlantCard = ({ plant }) => {
+    const history = useHistory()
+
     return (
         <div value={plant.id}>
             <h4>Common Name: {plant.plant_details.common_names}</h4>
             <p>Scientific Name: {plant.plant_details.scientific_name}</p>
             <p>{plant.plant_details.wiki_description.value}</p>
+            <img src={plant.similar_images[0].url}></img>
+            <button onClick={() => history.push("/create")}>This is my plant!</button>
         </div>
     )
 }
