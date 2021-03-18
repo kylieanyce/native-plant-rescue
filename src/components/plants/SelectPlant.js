1d950c73a687f1a6c2eca4803951cd3523f7bc93
kylieanyce <kylieanyce@gmail.com>
2021-03-18 15:28:55 -0500

    got plant select working

--- src/components/plants/SelectPlant.js
+++ src/components/plants/SelectPlant.js
@@ -5,10 +5,12 @@ import "./SelectPlant.css";
 export const SelectPlantCard = ({ plant }) => {
     const history = useHistory()
 
+    const plantCommonNames = plant.plant_details.common_names
+
     return (
         <div className="selectPlantCard" value={plant.id}>
             <h4>Scientific Name: {plant.plant_details.scientific_name}</h4>
-            {plant.plant_details.common_names !== null ? <p>Common Name: {plant.plant_details.common_names}</p> : ""}
+            {plantCommonNames !== null ? <p>Common Name: {plantCommonNames.map(item => item).join(", ")}</p> : ""}
             <p>{plant.plant_details.wiki_description.value}</p>
             <img src={plant.similar_images[0].url}></img>
             <button onClick={() => history.push("/create")}>This is my plant!</button>
