246e09227d24f63f66be52d336b08c64161d3576
kylieanyce <kylieanyce@gmail.com>
2021-03-19 20:28:33 -0500

    working on postProvider

--- src/components/posts/LibraryPost.js
+++ src/components/posts/LibraryPost.js
@@ -2,13 +2,22 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 
-export const LibraryPostCard = ({post}) => {
+export const LibraryPostCard = ({ post }) => {
     return (
         <section className="plantPosts" id={post.id}>
-            <h3>
-                {post.plant?.commonName}
-            </h3>
-            <p>{post.plant?.description}</p>
+            <div>
+                <h3>
+                    {post.plant?.scientificName}
+                </h3>
+                <h5>{post.plant?.commonName}</h5>
+                <p>{post.plant?.description}</p>
+                <img src={post.plant?.image}></img>
+            </div>
+            <div>
+                <h5>Available: {post.available === true ? "Yes" : "No"}</h5>
+                <p>Location: {post.address}</p>
+                <p>Details: {post.pickupInfo}</p>
+            </div>
         </section>
     )
 }
