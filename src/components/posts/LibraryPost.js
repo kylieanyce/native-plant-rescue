7c4e57cb1fdfd0c21b3184e538683d926a7e72cc
kylieanyce <kylieanyce@gmail.com>
2021-03-23 13:44:41 -0500

    library now only displays available plants

--- src/components/posts/LibraryPost.js
+++ src/components/posts/LibraryPost.js
@@ -5,18 +5,22 @@ import "./Library.css";
 
 export const LibraryPostCard = ({ post }) => {
     return (
-        <section className="postCards" id={post.id}>
-            <div>
-                <h3>
-                    {post.plant?.scientificName}
-                </h3>
-                <Link to={`/library/detail/${post.id}`}>
-                    <img className="image" src={post.plant?.image}></img>
-                </Link>
-            </div>
-            <div>
-                <h5>Available: {post.available === true ? "Yes" : "No"}</h5>
-            </div>
-        </section>
+        <>
+            {post.available === true ?
+                <section className="postCards" id={post.id}>
+                        <div>
+                            <h3>
+                                {post.plant?.scientificName}
+                            </h3>
+                            <Link to={`/library/detail/${post.id}`}>
+                                <img className="image" src={post.plant?.image}></img>
+                            </Link>
+                        </div>
+                        <div>
+                            <h5>Available: {post.available === true ? "Yes" : "No"}</h5>
+                        </div>
+                </section>
+                : ""}
+        </>
     )
 }
