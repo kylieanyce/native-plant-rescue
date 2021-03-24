7d03801b12b66c962675d03e995d411127df7b06
kylieanyce <kylieanyce@gmail.com>
2021-03-24 15:59:11 -0500

    finished commenting code for identify.js

--- src/components/plants/IdentifyForm.js
+++ src/components/plants/IdentifyForm.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { IdentifyContext } from "./Identify";
 import "./Identify.css";
 import { useHistory } from "react-router-dom";
@@ -7,13 +7,26 @@ import { useHistory } from "react-router-dom";
 export const IdentifyForm = () => {
     const { sendIdentification } = useContext(IdentifyContext)
     const history = useHistory()
+    // useRef grabs whatever associated file is input on the DOM 
+    // must be set to null until it is filled
+    const fileElement = useRef(null)
+
+    // when user clicks ok button, calls this function which
+    // grabs input from ref and sets to files variable then 
+    // passes files into sendIdentification
+    const handleFile = () => {
+        const files = fileElement.current.files
+        sendIdentification(files)
+    }
 
     return (
         <div className="outerIdentifyForm">
             <div className="identifyForm">
                 <form className="innerIdentifyForm">
-                    <input type="file" multiple />
-                    <button onClick={sendIdentification} type="button">OK</button>
+                    {/* ref needs to be associated with an input so the variable to be later 
+                    assigned with the input is added as a property */}
+                    <input type="file" ref={fileElement} multiple />
+                    <button onClick={handleFile} type="button">OK</button>
                 <button onClick={() => history.push(`/library`)}>Back to Plant Library</button>
                 </form>
 
