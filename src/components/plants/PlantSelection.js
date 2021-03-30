938162e0dd0ce0367bcecf5d592e1b65fc55de68
kylieanyce <kylieanyce@gmail.com>
2021-03-30 13:09:16 -0500

    select plant function on displays native plants

--- src/components/plants/PlantSelection.js
+++ src/components/plants/PlantSelection.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { IdentifyContext } from "./Identify";
 import { NativeContext } from "../natives/NativeProvider"
 import { SelectPlantCard } from "./SelectPlant"
@@ -7,21 +7,13 @@ import { SelectPlantCard } from "./SelectPlant"
 // gives user a selection of plants that were found to match theirs
 export const PlantSelection = () => {
     const { plants } = useContext(IdentifyContext)
-    const { TnNatives, getTnNatives } = useContext(NativeContext)
-
-    useEffect(() => {
-        getTnNatives()
-    }, [])
 
     return (
         <div>
             <h2>Choose Your Plant</h2>
             <section className="selectPlantList">
                 {plants.map(plant => {
-                    
-                    console.log(TnNatives)
-                    const filteredNatives = TnNatives.filter(nativeItem => nativeItem.scientificName === plant.plant_name)
-                    return <SelectPlantCard key={plant.id} plant={plant} native={filteredNatives}/>
+                    return <SelectPlantCard key={plant.id} plant={plant} />
                 })}
             </section>
         </div>
--- src/components/plants/SelectPlant.js
+++ src/components/plants/SelectPlant.js
@@ -1,31 +1,12 @@
 import React, { useContext } from "react"
-import { useHistory } from "react-router-dom"
 import "./SelectPlant.css";
 import { PlantContext } from "./PlantProvider"
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import CardActions from '@material-ui/core/CardActions';
-import { Grid } from '@material-ui/core';
 
-
-
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 345,
-    },
-    media: {
-        height: 140,
-    }
-});
 
 // renders individual matching plants on DOM and handles the select plant function
-export const SelectPlantCard = ({ plant, filteredNatives }) => {
+export const SelectPlantCard = ({ plant }) => {
+    console.log(plant)
     const { addPlant } = useContext(PlantContext)
     const newPlant = plant.plant_details.common_names ? plant.plant_details.common_names[0] : null
 
@@ -42,7 +23,6 @@ export const SelectPlantCard = ({ plant, filteredNatives }) => {
     }
 
     return (
-
         <div className="selectPlantCard" value={plant.id}>
             <h4 style={{ textTransform: 'capitalize' }}>Scientific Name: {plant.plant_details.scientific_name}</h4>
             <div className="imageAlign">
@@ -54,42 +34,7 @@ export const SelectPlantCard = ({ plant, filteredNatives }) => {
             {/* if the plant has no common names, this area will not display on DOM */}
             {plant.plant_details.common_names !== null ? <p style={{ textTransform: 'capitalize' }}>Common Name: {plant.plant_details.common_names[0]}</p> : ""}
             <p>{plant.plant_details.wiki_description.value}</p>
-            
-            
         </div>
-
-
-
-        // <Grid item xs={6} sm={4} md={3} lg={2}>
-        //     <Card className="selectPlantCard" value={plant.id} style={{ backgroundColor: "#13636e" }}>
-        //         <CardActionArea>
-        //             <CardMedia
-        //                 className={classes.media}
-        //                 image={plant?.similar_images[0].url}
-        //             />
-
-        //             <CardContent>
-        //                 <Typography gutterBottom variant="h5" component="h2" style={{ textTransform: 'capitalize', color: "#1e2745" }}>
-        //                     Scientific Name: {plant.plant_details.scientific_name}
-        //                 </Typography>
-
-        //                 <Typography variant="h6" color="textSecondary" component="p">
-        //                     {plant.plant_details.common_names !== null ? <p style={{ textTransform: 'capitalize' }}>Common Name: {plant.plant_details.common_names[0]}</p> : ""}
-        //                 </Typography>
-
-        //                 <Typography variant="body3" color="textSecondary" component="p">
-        //                     {plant.plant_details.wiki_description.value}
-        //                 </Typography>
-        //             </CardContent>
-
-        //             <CardActions>
-        //                 <Button onClick={handleCreatePost} variant="contained" size="small" style={{color: "#1e2745"}} >
-        //                     This is my plant!
-        //             </Button>
-        //             </CardActions>
-        //         </CardActionArea>
-        //     </Card>
-        // </Grid>
     )
 }
 
