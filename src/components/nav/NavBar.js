428223b46525c35f18d9423d18b4453f6daf19eb
kylieanyce <kylieanyce@gmail.com>
2021-04-01 11:16:41 -0500

    added different glow for hover on library posts

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -7,8 +7,8 @@ export const NavBar = (props) => {
     return (
         <>
             <ul className="navbar">
-                <li className="navbar__link"><Link to="/library">View Plant Library</Link></li>
                 <li className="navbar__link"><Link to="/myPosts">My Posts</Link></li>
+                <li className="navbar__link"><Link to="/library">View Plant Library</Link></li>
                 <li className="navbar__link"><Link to="/identifyForm">Identify A Plant</Link></li>
                 <li className="search"><SearchPost/></li>
             </ul>
